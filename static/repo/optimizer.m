5a5ea5941652c1e87bca716eb2d3817b1be51098
Fabrizio Sossan <fabriziosossan@Fabrizios-MacBook-Air.local>
2023-11-29 08:53:36 +0100

    latest build

--- static/repo/optimizer.m
+++ static/repo/optimizer.m
@@ -12,8 +12,16 @@ Enom = 10;
 Bmax = 5 * ones(T, 1);
 Bmin = -5 * ones(T, 1);
 SOC0 = 0.1;
-SOCMAX = 1 * ones(T, 1);
+SOCMAX = ones(T, 1)
 SOCMIN = 0 * ones(T, 1);
+
+Pmax = 5;
+
+
+L = 2 * ones(T, 1);
+L(17:20) = 3;
+
+
 
 
 SOCMAX(11:12) = 0.9;
@@ -53,6 +61,9 @@ b0 = SOCMAX - SOC0;
 A1 = -sigma * DeltaT / Enom;
 b1 = -(SOCMIN - SOC0);
 
+A2 = eye(T);
+b2 = Pmax * ones(T);
+
 A = vertcat(A0, A1);
 b = vertcat(b0, b1);
 
@@ -91,7 +102,7 @@ xlim([0, 23])
 title('SOC');
 
 
-nop
+%nop
 
 
 % Electricity bill
@@ -133,6 +144,28 @@ hold on
 stairs(time, P)
 legend('Power demand', 'Contractual power', 'Corrected power');
 hold on
+
+
+figure
+subplot(3, 1, 1)
+plot(time, c)
+ylim([0, max(c)*1.1])
+xticks([0:23])
+xlim([0, 23])
+title('Price');
+
+
+subplot(3, 1, 2)
+plot(time, x)
+xticks([0:23])
+xlim([0, 23])
+title('Battery power');
+
+subplot(3, 1, 3)
+plot(time, SOC)
+xticks([0:23])
+xlim([0, 23])
+title('SOC');
 
 
 
